728d2e2fa53fd5ea0922d44a2b4a8d106883812d
b27user12 <b27user12@rpsconsulting.in>
2025-07-05 15:32:30 +0530

    hi

--- roboshop-instance-create.sh
+++ roboshop-instance-create.sh
@@ -1,7 +1,7 @@
-ami_id="ami-09c813fb71547fc4f"
-instance_type="t2.micro"
-security_group-id="sg-0e431449e6b8a4604"
-zone_id="Z0022572U6LHZ3ASAGBB"
+AMI_ID="ami-09c813fb71547fc4f"
+INSTANCE_TYPE="t2.micro"
+SECURITY_GROUP_ID="sg-0e431449e6b8a4604"
+ZONE_ID="Z0022572U6LHZ3ASAGBB"
 
 instances=("frontend" "mongodb" "catalogue" "reids" "user" "cart" "shipping" "payment" "dispatch")
 
@@ -21,7 +21,7 @@ git clone https://github.com/Sameer-Sarrainodu/shell-roboshop.git
 
 for instance in "${instances[@]}"; do
   echo "Creating $instance instance"
-InstanceId=$(aws ec2 run-instances \
+INSTANCE_ID=$(aws ec2 run-instances \
     --image-id "$AMI_ID" \
     --instance-type "$INSTANCE_TYPE" \
     --security-group-ids "$SECURITY_GROUP_ID" \
@@ -29,7 +29,7 @@ InstanceId=$(aws ec2 run-instances \
     --query "Instances[0].InstanceId" \
     --output text)
 
-    echo "created instance with id $InstanceId"
+    echo "created instance with id $INSTANCE_ID"
 done
 
 
